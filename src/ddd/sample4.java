a3792bfa1906db7ac1af633c3b5349b89ef6fc82
Shantu-max <59841930+Shantu-max@users.noreply.github.com>
2020-01-21 16:28:52 +0530

    Update sample4.java

--- src/ddd/sample4.java
+++ src/ddd/sample4.java
@@ -3,7 +3,7 @@ package ddd;
 public class sample4 
 {
 	public static void main(String[] args) {
-		System.out.println("its a git hub push to midas from caremanagement project");
+		System.out.println("its a git hub push to midas from caremanagement project 1123");
 	}
 
 }
